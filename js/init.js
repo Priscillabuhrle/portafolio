c2bc5e4a8068c192e46bf25f42662aeac0cf545f
Priscilla <priscillabuhrlerocha@gmail.com>
2017-01-10 12:23:52 -0300

    se sube nuevamente por errores en gitbash, portafolio terminado, falta foto de perfil

---
+++ js/init.js
@@ -0,0 +1,13 @@
+//slaider portafolio
+$(document).ready(function(){
+	$('.slider').slider({full_width: true});
+});
+ //sobre mi
+ $(".button-collapse").sideNav();
+//conocimientos
+$(document).ready(function(){
+	$('.collapsible').collapsible();
+});
+
+
+ 
